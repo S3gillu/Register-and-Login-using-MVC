18b7ae2bc8a0052f4f1e171568ff66dc299c5724
S3gillu <shubhamsaurabh440@yahoo.com>
2017-09-27 15:00:04 +0530

    This is signup nad login form using MVC

---
+++ TaskMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TaskMvc.MvcApplication" Language="C#" %>
---
+++ TaskMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaskMvc.Startup))]
+namespace TaskMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
